e0dc8f46ab20e4bb6ea94eeda8a155b7eb8a6250
DB2 <DB2@DESKTOP-BNR6PG7>
2019-04-10 11:00:49 +0900

    upload수정

--- php_upload_test_2/upload.php
+++ php_upload_test_2/upload.php
@@ -1,8 +1,5 @@
-<?php
- echo "file upload program<br />";
- echo "select the file<br />";
-?>
+<a href="../php_upload_test_2/uploads/">File_List</a>
 <form method="post" action="upload2.php" enctype="multipart/form-data">
-<input type="file" size=100 name="upload"><hr>
-<input type="submit" value="send">
+<input type="submit" value="전송">
+<input type="file" name="upload" style="width: 1000px;height: 1000px;background-color:  rgb(198, 236, 224);">
 </form>
--- php_upload_test_2/upload2.php
+++ php_upload_test_2/upload2.php
@@ -1,16 +1,13 @@
 <?php
- echo "confirm file information <br />";
  $uploadfile = $_FILES['upload'] ['name'];
  $upload_dir = './uploads';
-
  if(move_uploaded_file($_FILES['upload']['tmp_name'],"$upload_dir/$uploadfile")){
-  echo "파일이 업로드 되었습니다.<br />";
-//   echo "<img src ={$_FILES['upload']['name']}> <p>";
-  echo "1. file name : {$_FILES['upload']['name']}<br />";
-  echo "2. file type : {$_FILES['upload']['type']}<br />";
-  echo "3. file size : {$_FILES['upload']['size']} byte <br />";
-  echo "4. temporary file name : {$_FILES['upload']['size']}<br />";
+  echo "파일이 업로드 되었습니다.<br>";
+  echo "file name : {$_FILES['upload']['name']}<br>";
  } else {
-  echo "파일 업로드 실패 !! 다시 시도해주세요.<br />";
+  echo "파일 업로드 실패 !! 다시 시도해주세요.<br>";
  }
-?>
+?> 
+<a href="../php_upload_test_2/uploads">File_List</a>
+<br>
+<a href="upload.php">GO_BACK</a>
