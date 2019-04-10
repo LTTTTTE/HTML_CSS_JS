5d6bc1ed4405e0ee36cebd788f3c2f6a4c429b3b
DB2 <DB2@DESKTOP-BNR6PG7>
2019-04-10 13:21:32 +0900

    test

--- php_upload_test_2/upload.php
+++ php_upload_test_2/upload.php
@@ -2,4 +2,5 @@
 <form method="post" action="upload2.php" enctype="multipart/form-data">
 <input type="submit" value="전송">
 <input type="file" name="upload" style="width: 1000px;height: 1000px;background-color:  rgb(198, 236, 224);">
+<br>
 </form>
